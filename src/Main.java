c5e658aac4266c808165eb3ed119d1c34e3177f8
Sparsh Aggarwal <sparshaggarwal8397@gmail.com>
2025-09-29 12:11:41 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        List<String> list = Arrays.asList("Apple","Banana","Watermelon","Mango");
+
+        // From a collection
+        Stream<String> stream1 = list.stream();
+
+
+
+    }
+}
